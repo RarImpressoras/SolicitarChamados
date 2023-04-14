6f050085e96987f3d7c0cb602c86a6217fbf5ca9
RarImpressoras <130790651+RarImpressoras@users.noreply.github.com>
2023-04-14 15:36:00 -0300

    Primeiro Commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDQNDiKqe4Et5FBg3gjEfn7Q8ocuZyb6cg",
+    authDomain: "rarc-6f934.firebaseapp.com",
+    projectId: "rarc-6f934",
+    storageBucket: "rarc-6f934.appspot.com",
+    messagingSenderId: "26113144877",
+    appId: "1:26113144877:web:19da59f57c43f92e40e0a4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
