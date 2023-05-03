bf99e6868adccdb226161b0c0bd02d5159db6110
RarImpressoras <130790651+RarImpressoras@users.noreply.github.com>
2023-05-03 16:34:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,38 @@ function toggleChat() {
   }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  createInitialMessage();
+});
+
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var serie = urlParams.get('serie');
+var modelo = urlParams.get('modelo');
+var empresa = urlParams.get('empresa');
+
+
+function createInitialMessage() {
+  const message = `Olá ${empresa}, selecione o problema da impressora ${modelo}:<br>
+<a href="#" onclick="atualizarTexto('1')">1 - Atolamento de papel ;</a><br>
+<a href="#" onclick="atualizarTexto('2')">2 - Solicitação de toner ;</a><br>
+<a href="#" onclick="atualizarTexto('3')">3 - Impressão manchando ;</a><br>
+<a href="#" onclick="atualizarTexto('4')">4 - Não imprime ;</a><br>
+<a href="#" onclick="atualizarTexto('5')">5 - Outros...</a>`;
+
+  const messageElement = document.createElement('div');
+  messageElement.className = 'received';
+  messageElement.innerHTML = message;
+  chatlog.appendChild(messageElement);
+  scrollChatLog();
+}
+
+function atualizarTexto(texto) {
+  chatinput.value = texto;
+}
+
+
+
 function addSentMessage(message) {
   const messageElement = document.createElement('div');
   messageElement.className = 'sent';
@@ -37,17 +69,34 @@ sendbutton.addEventListener('click', () => {
   const chatMessage = chatinput.value.toLowerCase();
   if (chatMessage.trim() !== '') {
     addSentMessage(chatMessage);
-    if (chatMessage === 'olá') {
+    if (chatMessage === "1") {
       setTimeout(() => {
         addReceivedMessage('Oi!');
         scrollChatLog();
       }, 1000);
-    } else if (chatMessage === 'tchau') {
+    } else if (chatMessage === "2") {
       setTimeout(() => {
         addReceivedMessage('Flw!');
         scrollChatLog();
       }, 1000);
-    } else {
+    }
+    else if (chatMessage === "3") {
+      setTimeout(() => {
+        addReceivedMessage('Oi!');
+        scrollChatLog();
+      }, 1000);
+    } else if (chatMessage === "4") {
+      setTimeout(() => {
+        addReceivedMessage('Flw!');
+        scrollChatLog();
+      }, 1000);
+    }
+    else if (chatMessage === "5") {
+      setTimeout(() => {
+        addReceivedMessage('Oi!');
+        scrollChatLog();
+      }, 1000);
+    }else {
       var mayta = "mayta"
       fetch("https://api.openai.com/v1/completions", {
         method: 'POST',
